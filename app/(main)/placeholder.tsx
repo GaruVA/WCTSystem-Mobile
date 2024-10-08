6626da8f02cd72ebd3339a83284f0ac512574689
GaruVA <garukaassalaarachchi@gmail.com>
2024-10-08 17:31:26 +0530

    Install necessary packages for authentication

--- app/(main)/placeholder.tsx
+++ app/(main)/placeholder.tsx
@@ -1,10 +1,12 @@
 import { Text, View } from 'react-native'
 import React from 'react'
+import { Link } from 'expo-router';
 
 export default function Settings() {
     return (
         <View className='flex-1 items-center justify-center'> 
             <Text>Hello, I'm just a placeholder.</Text>
+            <Link href="/">Go Back</Link>
         </View>
     );
 }
